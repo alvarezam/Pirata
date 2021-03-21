38c96b75107bdb6755a890dededa816b698b3cbf
alvarezam <72385380+alvarezam@users.noreply.github.com>
2021-03-21 23:26:49 +0100

    a

--- Assets/Scripts/Scr Menus/ScrMenu.cs
+++ Assets/Scripts/Scr Menus/ScrMenu.cs
@@ -5,6 +5,16 @@ using UnityEngine.SceneManagement;
 
 public class ScrMenu : MonoBehaviour
 {
+    /// <summary>
+    /// ----------------------------------------------------------------------------------
+    /// DESCRIPCIÓ
+    ///         Script utilitzat per controlar les accions dels botons del menu d'inici.
+    /// AUTOR:  Marc Alvarez Abril
+    /// DATA:   21/03/2021
+    /// VERSIÓ: 1.0
+    /// ----------------------------------------------------------------------------------
+    /// </summary>
+
     public void Link()
     {
         Application.OpenURL("https://www.emav.com/");
